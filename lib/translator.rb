7f64ad87c4b0b44abdca355ee3d26adff36b245b
Tien Tran <tientran2407.vn@gmail.com>
2018-10-03 06:39:37 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,6 +23,13 @@ def get_japanese_emoticon (file_path, emoticon)
   end
 end
 
-def get_english_meaning
+def get_english_meaning (file_path, emoticon)
   # code goes here
+   library = load_library(file_path)
+  result = library["get_meaning"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
